1872b9de861c9e9a71879cdd19a92a32ee251a26
Enrique <enriqueebal.15@gmail.com>
2024-02-13 11:44:39 -0700

    ok

--- src/DumpTibiaWiki/main.py
+++ src/DumpTibiaWiki/main.py
@@ -1,61 +1,12 @@
 import requests
+import sys
+sys.path.append('../lib')
 
+from clases import TibiaWiki
 
+def getData(TibiaWiki):
+    r = requests.get(f'https://tibiawiki.dev/api/{TibiaWiki.value}?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
+    with open(f"dump{TibiaWiki.value}.json", 'w') as archivo:
+        json.dump(datos, archivo)
 
-
-def getObjects():
-    r = requests.get('https://tibiawiki.dev/api/objects?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
-    with open("dumpObjects.json", 'w') as archivo:
-        json.dump(datos, archivo)
-        
-def getBooks():
-    r = requests.get('https://tibiawiki.dev/api/books?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
-    with open("dumpObjects.json", 'w') as archivo:
-        json.dump(datos, archivo)
-        
-def getAchievements():
-    r = requests.get('https://tibiawiki.dev/api/achievements?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
-    with open("dumpObjects.json", 'w') as archivo:
-        json.dump(datos, archivo)
-        
-def getMounts():
-    r = requests.get('https://tibiawiki.dev/api/mounts?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
-    with open("dumpObjects.json", 'w') as archivo:
-        json.dump(datos, archivo)
-        
-def getMissiles():
-    r = requests.get('https://tibiawiki.dev/api/missiles?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
-    with open("dumpObjects.json", 'w') as archivo:
-        json.dump(datos, archivo)
-        
-def getEffects():
-    r = requests.get('https://tibiawiki.dev/api/effects?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
-    with open("dumpObjects.json", 'w') as archivo:
-        json.dump(datos, archivo)
-        
-def getCharms():
-    r = requests.get('https://tibiawiki.dev/api/charms?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
-    with open("dumpObjects.json", 'w') as archivo:
-        json.dump(datos, archivo)
-        
-def getNpcs():
-    r = requests.get('https://tibiawiki.dev/api/npcs?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
-    with open("dumpObjects.json", 'w') as archivo:
-        json.dump(datos, archivo)
-        
-def getCreatures():
-    r = requests.get('https://tibiawiki.dev/api/creatures?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
-    with open("dumpObjects.json", 'w') as archivo:
-        json.dump(datos, archivo)
-        
-def getBuildings():
-    r = requests.get('https://tibiawiki.dev/api/creatures?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
-    with open("dumpObjects.json", 'w') as archivo:
-        json.dump(datos, archivo)
-        
-def getCreatures():
-    r = requests.get('https://tibiawiki.dev/api/creatures?expand=true') # OBTENER LISTA COMPLETA DE ITEMS
-    with open("dumpObjects.json", 'w') as archivo:
-        json.dump(datos, archivo)
-        
-        
+getData(TibiaWiki.Books)
